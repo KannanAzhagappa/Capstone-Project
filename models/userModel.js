f0d166a19e738e96176407affcef3cfda27a2d72
KannanAzhagappa <58419883+KannanAzhagappa@users.noreply.github.com>
2021-03-04 13:47:24 +0800

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    root: {
+      type: Boolean,
+      default: false,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/kannanazhagappa/image/upload/v1614752034/ecommerce_media/avatar_gquslj.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let Dataset = mongoose.models.user || mongoose.model("user", userSchema);
+export default Dataset;
